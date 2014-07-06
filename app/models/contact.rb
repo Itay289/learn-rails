418f778b1f063f3afce49e3b5b2d4fff7abc6871
Itay Benari <itay289@gmail.com>
2014-07-06 22:35:38 +0300

    save data to a spreadsheet

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,11 +5,29 @@ class Contact < ActiveRecord::Base
   column :email, :string
   column :content, :string
 
-  validates_presence_of :name
-  validates_presence_of :email
-  validates_presence_of :content
-  validates_presence_of :email, 
-    :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
-  validates_length_of :content, :maximum => 500  
-      
-end
+  validates :name, presence: true
+  validates :email, presence: true,
+  presence: { with: /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i }
+  validates :content, presence: true,
+  length: { maximum: 500 }
+
+  def update_spreadsheet
+    connection = GoogleDrive.login(Rails.application.secrets.email_provider_username,
+      Rails.application.secrets.email_provider_password)
+
+    ss = connection.spreadsheet_by_title('Learn-Rails-Exaple')
+    if ss.nil?
+      ss = connection.create_spreadsheet('Learn-Rails-Exaple')
+    end
+    ws = ss.worksheets[0]
+    last_row = 1 + ws.num_rows
+    ws[last_row, 1] = Time.new
+    ws[last_row, 2] = self.name
+    ws[last_row, 3] = self.email
+    ws[last_row, 4] = self.content
+    ws.save
+  end
+
+end      
+
+          
